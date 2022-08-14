6431ae4df07e82ebacb8cdd43f032e2c7f18382e
Paul <paulhyunchong@gmail.com>
2022-08-14 13:02:05 -0700

    Adds favicons

--- components/SEO.js
+++ components/SEO.js
@@ -29,6 +29,32 @@ const CommonSEO = ({ title, description, ogType, ogImage, twImage }) => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={twImage} />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/assets/favicon/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/assets/favicon/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/assets/favicon/android-chrome-192x192.png"
+        sizes="192x192"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/assets/favicon/android-chrome-512x512.png"
+        sizes="512x512"
+      />
+      <link rel="apple-touch-icon" href="/assets/favicon/apple-touch-icon" />
+      <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
     </Head>
   );
 };
