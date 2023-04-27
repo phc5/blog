c17ce5394bf243377d07cfe004fcd15a18919739
Paul <paulhyunchong@gmail.com>
2023-04-26 22:08:33 -0700

    Removes panelbear since it is deprecated

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import Router from 'next/router';
 import NProgress from 'nprogress';
 
 import LayoutWrapper from '@/components/layouts/LayoutWrapper';
-import { usePanelBear } from '../hooks/usePanelbear';
 import siteMetadata from '@/data/siteMetadata';
 import '@/styles/index.css';
 import '@/styles/prism.css';
@@ -14,10 +13,6 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
-  usePanelBear(process?.env?.NEXT_PUBLIC_PANEL_BEAR_ID, {
-    // Uncomment to allow sending events on localhost, and log to console too.
-    debug: false,
-  });
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
